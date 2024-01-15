01c50d8bbd9b9a918965e9b190466932f77bcea3
indiscrete_void <indiscrete.void@gmail.com>
2024-01-15 05:56:28 +0100

    cleanup Polysemy.TTY.IO

--- cli/Polysemy/TTY/IO.hs
+++ cli/Polysemy/TTY/IO.hs
@@ -23,17 +23,15 @@ protoSize = maybeProtoSize >>= maybeFail "unable to get terminal size"
       where
         w2s (Window h w) = (w, h)
 
-wrapHandlerS :: (Functor f) => (Size -> n b) -> Sem (WithStrategy IO f n) (IO b)
+wrapHandlerS :: (Functor f) => (Size -> n a) -> Sem (WithStrategy IO f n) (IO a)
 wrapHandlerS f = liftM3 wrapper (bindS f) getInspectorS getInitialStateS
   where
-    wrapper h ins s = protoSizeS >>= maybeFail "unable to inspect SIGWINCH handler result"
-      where
-        protoSizeS = inspect ins <$> (protoSize >>= h . (<$ s))
+    wrapper f' ins s = (protoSize >>= f' . (<$ s)) >>= maybeFail "unable to inspect SIGWINCH handler result" . inspect ins
 
 wrapBracketActionS :: n a -> Sem (WithStrategy IO f n) (IO a)
-wrapBracketActionS ma = liftM2 wrapper (runS ma) getInspectorS
+wrapBracketActionS m = liftM2 wrapper (runS m) getInspectorS
   where
-    wrapper mb ins = mb >>= maybeFail "unable to inspect bracket action result" . inspect ins
+    wrapper m' ins = m' >>= maybeFail "unable to inspect bracket action result" . inspect ins
 
 withRaw :: TerminalAttributes -> TerminalAttributes
 withRaw attrs = foldr (flip withoutMode) attrs [EnableEcho, IgnoreBreak, InterruptOnBreak, IgnoreParityErrors, MarkParityErrors, CheckParity, StripHighBit, MapLFtoCR, IgnoreCR, MapCRtoLF, StartStopOutput, StartStopInput, ProcessInput, ProcessOutput, KeyboardInterrupts]
@@ -45,11 +43,11 @@ ttyToIOFinal :: (Member (Final IO) r) => Fd -> Fd -> InterpreterFor TTY r
 ttyToIOFinal i o = interpretFinal @IO $ \case
   (SetResizeHandler f) -> wrapHandlerS f >>= liftS . go
     where
-      go g = void $ installHandler sigWINCH (Catch g) Nothing
+      go f' = void $ installHandler sigWINCH (Catch f') Nothing
   GetSize -> liftS protoSize
-  (AttributeBracket ma) -> wrapBracketActionS ma >>= liftS . go
+  (AttributeBracket m) -> wrapBracketActionS m >>= liftS . go
     where
-      go mb = bracket (getTerminalAttributes i) (setTerminalAttributesImmediately i) (const mb)
+      go m' = bracket (getTerminalAttributes i) (setTerminalAttributesImmediately i) (const m')
   SetRaw -> liftS $ getTerminalAttributes i >>= setTerminalAttributesImmediately i . withRaw
   Read -> liftS $ eofToNothing <$> fdRead i 8192
   (Write str) -> liftS . void $ fdWrite o str
