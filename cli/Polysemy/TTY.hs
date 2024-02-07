12215506986216bf40ce096de4fe9e4dc3897514
indiscrete_void <indiscrete.void@gmail.com>
2024-02-07 20:43:44 +0100

    move all helper functions for ttyToIOFinal to it's where clauses

--- cli/Polysemy/TTY.hs
+++ cli/Polysemy/TTY.hs
@@ -10,6 +10,7 @@ where
 
 import Control.Exception
 import Control.Monad
+import Data.Int
 import IOSH.Maybe
 import IOSH.Protocol
 import Polysemy
@@ -28,34 +29,26 @@ data TTY m a where
 
 makeSem ''TTY
 
-protoSize :: IO (Maybe Size)
-protoSize = fmap w2s <$> size
-  where
-    w2s (Window h w) = (w, h)
-
-wrapHandlerS :: (Functor f) => (Size -> n ()) -> Sem (WithStrategy IO f n) (IO ())
-wrapHandlerS f = liftM3 wrapper (bindS f) getInspectorS getInitialStateS
-  where
-    wrapper f' ins s = (protoSize >>= maybe (pure . void $ s) (f' . (<$ s))) >>= maybeFail "unable to inspect SIGWINCH handler result" . inspect ins
-
-wrapBracketActionS :: n a -> Sem (WithStrategy IO f n) (IO a)
-wrapBracketActionS m = liftM2 wrapper (runS m) getInspectorS
-  where
-    wrapper m' ins = m' >>= maybeFail "unable to inspect bracket action result" . inspect ins
-
-withRaw :: TerminalAttributes -> TerminalAttributes
-withRaw attrs = foldr (flip withoutMode) attrs [EnableEcho, IgnoreBreak, InterruptOnBreak, IgnoreParityErrors, MarkParityErrors, CheckParity, StripHighBit, MapLFtoCR, IgnoreCR, MapCRtoLF, StartStopOutput, StartStopInput, ProcessInput, ProcessOutput, KeyboardInterrupts]
-
-setTerminalAttributesImmediately :: Fd -> TerminalAttributes -> IO ()
-setTerminalAttributesImmediately fd attrs = setTerminalAttributes fd attrs Immediately
-
 ttyToIOFinal :: (Member (Final IO) r) => Fd -> InterpreterFor TTY r
 ttyToIOFinal term = interpretFinal @IO $ \case
-  (SetResizeHandler f) -> wrapHandlerS f >>= liftS . go
+  (SetResizeHandler f) -> wrapHandlerS >>= liftS . go
     where
       go f' = void $ installHandler sigWINCH (Catch f') Nothing
+      wrapHandlerS = liftM3 wrapper (bindS f) getInspectorS getInitialStateS
+        where
+          wrapper f' ins s = (protoSize >>= maybe (pure . void $ s) (f' . (<$ s))) >>= maybeFail "unable to inspect SIGWINCH handler result" . inspect ins
   GetSize -> liftS protoSize
-  (AttributeBracket m) -> wrapBracketActionS m >>= liftS . go
+  (AttributeBracket m) -> wrapBracketActionS >>= liftS . go
     where
       go m' = bracket (getTerminalAttributes term) (setTerminalAttributesImmediately term) (const m')
+      wrapBracketActionS = liftM2 wrapper (runS m) getInspectorS
+        where
+          wrapper m' ins = m' >>= maybeFail "unable to inspect bracket action result" . inspect ins
   SetRawAttributes -> liftS $ getTerminalAttributes term >>= setTerminalAttributesImmediately term . withRaw
+    where
+      withRaw attrs = foldr (flip withoutMode) attrs [EnableEcho, IgnoreBreak, InterruptOnBreak, IgnoreParityErrors, MarkParityErrors, CheckParity, StripHighBit, MapLFtoCR, IgnoreCR, MapCRtoLF, StartStopOutput, StartStopInput, ProcessInput, ProcessOutput, KeyboardInterrupts]
+  where
+    setTerminalAttributesImmediately fd attrs = setTerminalAttributes fd attrs Immediately
+    protoSize = fmap w2s <$> size @Int16
+      where
+        w2s (Window h w) = (w, h)
