e3cbeb32dfd9c891e7bbfe30135033dc0f857690
indiscrete_void <indiscrete.void@gmail.com>
2024-02-09 04:59:39 +0100

    refactor cli main

--- cli/IOSH/Main.hs
+++ cli/IOSH/Main.hs
@@ -1,5 +1,6 @@
 import Control.Monad
 import Data.Bool
+import Debug.Trace
 import IOSH.Options
 import IOSH.Protocol
 import Pipes hiding (await)
@@ -27,31 +28,32 @@ serverMessageReceiver = runEffect $ for xReader go
   where
     go (Output str) = lift $ write str
     go (Error str) = lift $ writeErr str
-    go (Termination code) = lift $ exit code
+    go (Termination code) = lift $ exit (traceShowId code)
 
 ttyOutputSender :: (Member Process r, Member User r) => Sem r ()
 ttyOutputSender = runEffect $ reader >-> P.map Input >-> xWriter
 
-ptyIOSH :: (Member Process r, Member Decoder r, Member TTY r, Member Async r, Member User r, Member Fail r) => Maybe Environment -> FilePath -> Args -> Sem r ()
-ptyIOSH maybeEnv path args = rawBracket $ do
-  getSize >>= writeX . Handshake True maybeEnv path args
+ptyInit :: (Member Process r, Member TTY r) => FilePath -> Args -> Maybe Environment -> Sem r ()
+ptyInit path args sessionEnv = rawBracket $ do
+  getSize >>= writeX . Handshake True sessionEnv path args
   setResizeHandler (writeX . Resize)
-  async_ ttyOutputSender
-  serverMessageReceiver
 
-procIOSH :: (Member Process r, Member Decoder r, Member Async r, Member User r, Member Fail r) => Maybe Environment -> FilePath -> Args -> Sem r ()
-procIOSH maybeEnv path args = do
-  writeX $ Handshake False maybeEnv path args Nothing
-  async_ ttyOutputSender
-  serverMessageReceiver
+procInit :: (Member Process r) => FilePath -> Args -> Maybe Environment -> Sem r ()
+procInit path args sesionEnv = do
+  writeX $ Handshake False sesionEnv path args Nothing
+
+init :: (Member Process r, Member TTY r) => Bool -> FilePath -> Args -> Maybe Environment -> Sem r ()
+init = bool procInit ptyInit
+
+getSessionEnv :: (Member User r) => Bool -> Sem r (Maybe Environment)
+getSessionEnv = bool (pure Nothing) (Just <$> getEnv)
 
 iosh :: (Member (Scoped ProcessParams Process) r, Member Async r, Member TTY r, Member User r, Member Decoder r, Member Fail r) => Options -> Sem r ()
 iosh (Options pty inheritEnv tunProcCmd path args) =
   exec (TunnelProcess tunProcCmd) $ do
-    maybeEnv <- bool (pure Nothing) (Just <$> getEnv) inheritEnv
-    if pty
-      then ptyIOSH maybeEnv path args
-      else procIOSH maybeEnv path args
+    getSessionEnv inheritEnv >>= init pty path args
+    async_ ttyOutputSender
+    serverMessageReceiver
 
 main :: IO ()
 main = execOptionsParser >>= run
