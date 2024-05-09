7c3f1507bdf235bcfd8d3ef708e7a28fe40ae331
indiscrete_void <indiscrete.void@gmail.com>
2024-05-09 12:34:07 +0200

    extract Polysemy.Resize

--- daemon/Polysemy/PTY.hs
+++ daemon/Polysemy/PTY.hs
@@ -10,7 +10,6 @@ module Polysemy.PTY
 where
 
 import Control.Monad
-import Data.Bifunctor
 import Data.Kind
 import IOSH.Protocol hiding (Input, Output, Resize)
 import Polysemy
@@ -18,6 +17,7 @@ import Polysemy.Bundle
 import Polysemy.Close
 import Polysemy.Input
 import Polysemy.Output
+import Polysemy.Resize
 import Polysemy.Resource
 import Polysemy.Scoped
 import Polysemy.ScopedBundle
@@ -32,17 +32,11 @@ import Prelude hiding (read)
 type PTYParams :: Type
 data PTYParams = PTYParams (Maybe Environment) FilePath Args Size
 
-type Resize :: Effect
-data Resize m a where
-  Resize :: Size -> Resize m ()
-
 type PTYEffects :: [Effect]
 type PTYEffects = Resize ': ByteInputWithEOF ': ByteOutput ': Wait ': Close ': '[]
 
 type PTY :: Effect
 type PTY = Bundle PTYEffects
-
-makeSem ''Resize
 
 bundlePTYEffects :: (Member PTY r) => InterpretersFor PTYEffects r
 bundlePTYEffects =
@@ -54,9 +48,6 @@ bundlePTYEffects =
 
 exec :: (Member (Scoped PTYParams PTY) r) => PTYParams -> InterpretersFor PTYEffects r
 exec params = scoped @_ @PTY params . bundlePTYEffects . insertAt @5 @'[PTY]
-
-ps2s :: Size -> (Int, Int)
-ps2s = join bimap fromIntegral
 
 scopedPTYToIOFinal :: (Member (Final IO) r) => InterpreterFor (Scoped PTYParams PTY) r
 scopedPTYToIOFinal = runScopedBundle ptyParamsToIOFinal
@@ -87,7 +78,3 @@ inputToPtyIO pty = interpret \case
 outputToPtyIO :: (Member (Embed IO) r) => Pty -> InterpreterFor ByteOutput r
 outputToPtyIO pty = interpret \case
   Output str -> embed $ threadWaitWritePty pty >> writePty pty str
-
-resizeToIO :: (Member (Embed IO) r) => Pty -> InterpreterFor Resize r
-resizeToIO pty = interpret \case
-  Resize size -> embed $ resizePty pty (ps2s size)
